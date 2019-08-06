a559520540a53897534f21839ad7440aaf4bc1da
siddhesh <siddheshdagade77@gmail.com>
2019-08-06 12:25:31 +0530

    todo list add function

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
 import Counter from "./components/Counter";
+import Todolist from "./components/todolist"
 
 class App extends Component{
     render(){
         return(<div>
-            <Counter name="siddhesh"/>
+            {/* <Counter name="siddhesh"/>
             <Counter name="sai"/>
             <Counter name="aditya"/>
-            <Counter name="priyanka"/>
+            <Counter name="priyanka"/> */}
+            <Todolist />
+
             </div>
         );
     }
--- myfirstreact/src/components/Counter.js
+++ myfirstreact/src/components/Counter.js
@@ -18,7 +18,7 @@ class Counter extends Component{
         });
     
     }
-zdsdsdsdsd
+
 
 
     render(){
