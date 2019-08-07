565bb8d6031b6c34fea30f995ebbd5b8d4ac2df7
siddhesh <siddheshdagade77@gmail.com>
2019-08-07 13:32:45 +0530

    todo list added some css in list

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -2,17 +2,18 @@ import React, {Component} from "react";
 import Counter from "./components/Counter";
 import Todolist from "./components/todolist";
 import TTdoolist from "./components/Todolist1";
+import "./App.css"
 
 
 class App extends Component{
     render(){
-        return(<div>
+        return(<div className="todo">
             {/* <Counter name="siddhesh"/>
             <Counter name="sai"/>
             <Counter name="aditya"/>
             <Counter name="priyanka"/> */}
             {/* <Todolist /> */}
-            <TTdoolist />
+            <TTdoolist className="todoList" />
 
             </div>
         );
--- myfirstreact/src/components/Todolist1.js
+++ myfirstreact/src/components/Todolist1.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+import "./Todolist1.css"
+
 class TToodolist extends Component{
     constructor(props){
         super(props);
@@ -9,8 +12,11 @@ class TToodolist extends Component{
         this.addList = this.addList.bind(this);
         this.updateInputValue = this.updateInputValue.bind(this);
         this.remove = this.remove.bind(this);
+        this.strike = this.strike.bind(this);
     }
+    strike(i){
 
+    }
     updateInputValue(s){
         this.setState({inputValue : s.target.value})
     }
@@ -22,6 +28,7 @@ class TToodolist extends Component{
             list: newList ,
             inputValue : ""})
     }
+
     remove(i) {
         console.log('i', i);
         console.log('this item', this.state.list[i])
@@ -32,28 +39,33 @@ class TToodolist extends Component{
         })
     }
     render(){
-        return(<div>
-            <input id="input" 
-                       type="text" 
-                       value={this.state.inputValue} 
-                       onChange={this.updateInputValue}
-                       
-                       autoComplete="off"
-                />
+        return(<div className="mainClass">
+                    <div className="buttonClass">
+                        <input id="input" 
+                            type="text" 
+                            value={this.state.inputValue} 
+                            onChange={this.updateInputValue}
+                            
+                            autoComplete="off"
+                        />
 
-                <button type="button" 
-                        onClick={this.addList}>
-                        Add Me
-                </button>
-                <div><ul>
-                        {this.state.list.map((item, index) => 
-                            <li key={index}>
-                                <span>{item}</span>
-                                <button onClick={() => this.remove(index)}>x</button>
-                            </li>
-                        )}        
-                    </ul></div>
-        </div>);
+                        <button type="button" 
+                                onClick={this.addList}>
+                                Add Me
+                        </button>
+                    </div>
+                    <div className="listClass">
+                        <ul>
+                            {this.state.list.map((item, index) => 
+                                <li key={index}>
+                                    <button onClick={() => this.strike(index)}>✓</button>
+                                    <span>{item}</span>
+                                    <button onClick={() => this.remove(index)}>x</button>
+                                </li>
+                            )}        
+                        </ul>
+                    </div>
+                </div>);
     }
 }
 export default TToodolist;
